701934018238ba1bbefc6b99df088633021bcbc5
Bong <songning.lan@gmail.com>
2023-03-09 16:03:47 -0500

    Initial Commit

---
+++ Main.ino
@@ -0,0 +1,23 @@
+//Changez les noms au besoin:
+int pin1 = 1;
+int pin2 = 2;
+int pin3 = 3;
+int pin4 = 4;
+int pin5 = 5;
+int pin6 = 6;
+int pin7 = 7;
+int pin8 = 8;
+int pin9 = 9;
+int pin10 = 10;
+int pin11 = 11;
+int pin12 = 12;
+int pin14 = 13;
+
+void setup(){
+    
+    Serial.begin(9600);
+}
+
+void loop(){
+
+}
