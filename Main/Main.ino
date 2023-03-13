3cc47a42dddaed951458376f82f1058c4beeeead
Bong <songning.lan@gmail.com>
2023-03-13 09:49:51 -0400

    test

--- Main/Main.ino
+++ Main/Main.ino
@@ -11,16 +11,17 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-int pin1;
-int pin2;
-int pin3;
-int pin4;
+int pin1=1;
+int pin2=2;
+int pin3=3;
+int pin4=4;
+ServoMoteur moteur(servo1, servo2, servo3, servo4, pin1, pin2, pin3, pin4);
 
 void setup(){
-  ServoMoteur(servo1, servo2, servo3, servo4, pin1, pin2, pin3, pin4);
+  
   Serial.begin(9600);
 }
 
 void loop(){
-  Forward
+  moteur.Forward();
 }
